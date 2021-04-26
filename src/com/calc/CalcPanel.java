eb96cb682ab4834dc8362fcbb0db8a8ecee16f2e
d_nekh <dnekh2000@gmail.com>
2021-04-27 05:59:20 +0900

    CalcPanel is done/actions and logic are next

--- src/com/calc/CalcPanel.java
+++ src/com/calc/CalcPanel.java
@@ -1,6 +1,46 @@
 package com.calc;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class CalcPanel extends JPanel {
+
+    JPanel panel;
+
+    public CalcPanel() {
+        super();
+        setLayout(new BorderLayout());
+        JButton display = new JButton("0");
+        display.setEnabled(false);
+        add(display, BorderLayout.NORTH);
+
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4));
+        addButton("7");
+        addButton("8");
+        addButton("9");
+        addButton("/");
+
+        addButton("4");
+        addButton("5");
+        addButton("6");
+        addButton("*");
+
+        addButton("1");
+        addButton("2");
+        addButton("3");
+        addButton("-");
+
+        addButton("0");
+        addButton(".");
+        addButton("=");
+        addButton("+");
+
+        add(panel, BorderLayout.CENTER);
+    }
+
+    private void addButton(String label) {
+        JButton button = new JButton(label);
+        panel.add(button);
+    }
 }
--- src/com/calc/Calculator.java
+++ src/com/calc/Calculator.java
@@ -10,8 +10,8 @@ public class Calculator {
         frame.setTitle("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(430, 200, 640, 480);
-        //CalcPanel panel = new CalcPanel();
-        //frame.add(panel);
+        CalcPanel panel = new CalcPanel();
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
